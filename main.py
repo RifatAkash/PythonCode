20d3d6a459f7f32fc22bd365bf9901ee6c3254dc
Rifat Akash <32797541+RifatAkash@users.noreply.github.com>
2022-07-13 19:37:46 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,112 +1,34 @@
-class Person(object):
+#def myFun(*argv):
+#    for arg in argv:
+#       print(arg)
+# myFun("ok" , "there", "you", "go")
 
-    def __init__(self,name, id):
-        self.name = name
-        self.id =id
-    def Diaplay(self):
-        print(self.name, self.id)
-emp = Person("akash",404)
-emp.Diaplay()
-class Emp(Person):
-    def Print(self):
-        print("Emp class called")
-Emp_details = Emp("Akash", 201)
-Emp_details.Diaplay()
-Emp_details.Print()
-
-class Person(object):
-
-	def __init__(self, name):
-		self.name = name
-	def getName(self):
-		return self.name
-
-	def isEmployee(self):
-		return False
-
-class Employee(Person):
-	def isEmployee(self):
-		return True
-
-emp = Person("Mr")
-print(emp.getName(), emp.isEmployee())
-
-emp = Employee("Mrs")
-print(emp.getName(), emp.isEmployee())
-
-class Person(object):
-	def __init__(self, name, idnumber):
-		self.name = name
-		self.idnumber = idnumber
-
-	def display(self):
-		print(self.name)
-		print(self.idnumber)
-
-class Employee(Person):
-	def __init__(self, name, idnumber, salary, post):
-		self.salary = salary
-		self.post = post
-		Person.__init__(self, name, idnumber)
-
-a = Employee('Rifat', 1602060, 12000, "Intern")
-
-a.display()
-#for multiple inheritance
-
-class Base1(object):
-	def __init__(self):
-		self.str1 = "Vivasoft"
-		print("Base1")
+#def myFun(arg1, *argv):
+#	print(arg1)
+#	for arg in argv:
+#		print(arg)
 
 
-class Base2(object):
-	def __init__(self):
-		self.str2 = "Limited"
-		print("Base2")
+#myFun("ok" , "there", "you", "go")
+def myFun(**kwargs):
+	for key, value in kwargs.items():
+		print("%s == %s" % (key, value))
 
 
-class Derived(Base1, Base2):
-	def __init__(self):
+myFun(first='hey', mid='you', last='there')
+def myFun(arg1, arg2, arg3):
+	print("arg1:", arg1)
+	print("arg2:", arg2)
+	print("arg3:", arg3)
+args = ("hey", "you", "there")
+myFun(*args)
 
-		Base1.__init__(self)
-		Base2.__init__(self)
-		print("Derived")
+kwargs = {"arg1": "now", "arg2": "i can", "arg3": "see you"}
+myFun(**kwargs)
+def myFun(*args, **kwargs):
+	print("args: ", args)
+	print("kwargs: ", kwargs)
 
-	def printStrs(self):
-		print(self.str1, self.str2)
-
-
-ob = Derived()
-ob.printStrs()
-
-###multilevel
-class Base(object):
-
-	# Constructor
-	def __init__(self, name):
-		self.name = name
-
-	# To get name
-	def getName(self):
-		return self.name
-
-class Child(Base):
-	def __init__(self, name, age):
-		Base.__init__(self, name)
-		self.age = age
-	def getAge(self):
-		return self.age
-
-class GrandChild(Child):
-	def __init__(self, name, age, address):
-		Child.__init__(self, name, age)
-		self.address = address
-
-	def getAddress(self):
-		return self.address
-
-g = GrandChild("Rifat", 26, "Vivasoft")
-print(g.getName(), g.getAge(), g.getAddress())
+myFun('i', 'am', 'nobody', first="who", mid="you", last="are")
 
 
